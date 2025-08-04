1ca340bcf1d60ebba47eadd65a76d6a11020a8c8
Wysted <vladimirbesser@gmail.com>
2025-08-04 15:09:45 -0400

    [ADD] Cron locker's

--- src/cmd/cron/cron.go
+++ src/cmd/cron/cron.go
@@ -2,10 +2,15 @@ package cron
 
 import (
 	"log"
+	"time"
 
 	"github.com/CPU-commits/Template_Go-EventDriven/src/cmd/bus/queue"
+	"github.com/CPU-commits/Template_Go-EventDriven/src/package/db"
 	"github.com/CPU-commits/Template_Go-EventDriven/src/package/logger"
 	"github.com/CPU-commits/Template_Go-EventDriven/src/publication/controller"
+
+	"github.com/go-redsync/redsync/v4"
+	goredis "github.com/go-redsync/redsync/v4/redis/goredis/v9"
 	"github.com/robfig/cron/v3"
 )
 
@@ -15,7 +20,28 @@ func Init(logger logger.Logger) {
 
 	cronPublication := controller.NewCronPublication(bus)
 
-	_, err := c.AddFunc("@every 30m", cronPublication.UpdateRatings)
+	pool := goredis.NewPool(db.RClient)
+	rs := redsync.New(pool)
+	_, err := c.AddFunc("@every 30m", func() {
+		mutex := rs.NewMutex("cron-update-ratings",
+			redsync.WithExpiry(40*time.Minute),
+			redsync.WithTries(1),
+		)
+
+		if err := mutex.Lock(); err != nil {
+			log.Fatalf("Failed to schedule cronjob: %v", err)
+		}
+
+		cronPublication.UpdateRatings()
+
+		if ok, err := mutex.Unlock(); !ok || err != nil {
+			log.Fatalf("Failed to schedule cronjob: %v", err)
+
+		} else {
+			log.Fatalf("success cron-update-ratings")
+
+		}
+	})
 	if err != nil {
 		log.Fatalf("Failed to schedule cronjob: %v", err)
 	}
